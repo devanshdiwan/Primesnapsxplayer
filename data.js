4a757917bcf1b1ab724dc3811059994cfccab21a
Devansh Diwan <devanshdiwan97@gmail.com>
2025-11-13 15:13:39 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+// YEH AAPKA DATABASE HAI
+// Har video ki details yahan add karni hai.
+// Admin panel se code generate karke iske neeche paste karte jao.
+
+const videos = {
+    "sample-video": {
+        title: "Sample Video Title",
+        videoLink: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        posterLink: "https://i.imgur.com/8pXnU0b.jpg",
+        server2Link: "https://example.com/server-2",
+        adultLink: "https://example.com/adult-link"
+    },
+    
+    "ddd": {
+        title: "ddd",
+        videoLink: "https://drive.google.com/file/d/1-0HWU7S9nfWFYzatAqPBiZmxYIAhZLRo/view?usp=sharing",
+        posterLink: "https://drive.google.com/file/d/1-0HWU7S9nfWFYzatAqPBiZmxYIAhZLRo/view?usp=sharing",
+        server2Link: "https://drive.google.com/file/d/1-0HWU7S9nfWFYzatAqPBiZmxYIAhZLRo/view?usp=sharing",
+        adultLink: "https://drive.google.com/file/d/1-0HWU7S9nfWFYzatAqPBiZmxYIAhZLRo/view?usp=sharing"
+    },
+    
+};
